5f4849ad3b8135acccd30c4ed297f4bcf4ea3581
Nirnay S <96634165+nirsad@users.noreply.github.com>
2022-04-02 00:41:34 -0400

    Fixed spacing inconsistencies

--- mongodb/mongoingest.py
+++ mongodb/mongoingest.py
@@ -6,9 +6,9 @@ database = cluster['ArticleData']
 new_collection_name = date.today().strftime("%b_%d_%Y")
 collection = database[new_collection_name]
 with open(os.path.join(os.getcwd(), '..','article_data.csv'), 'r') as csvfile:
-	reader = csv.reader(csvfile, delimiter = ',')
-	for id, row in enumerate(reader):
-		if row[0] == 'Title' and row[1] == 'Authors':
-			pass
-		else:
-			collection.insert_one({'_id':id, 'Title':row[0], 'Authors':row[1], 'Citations':row[2]}) 
+    reader = csv.reader(csvfile, delimiter = ',')
+    for id, row in enumerate(reader):
+        if row[0] == 'Title' and row[1] == 'Authors':
+            pass
+        else:
+            collection.insert_one({'_id':id, 'Title':row[0], 'Authors':row[1], 'Citations':row[2]}) 
